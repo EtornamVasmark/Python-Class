3d982ed92f57fda08cc3e010aad2c7e9d14510a5
Etornam Vasmark-Voegborlo <etornamvas24@gmail.com>
2023-11-28 00:47:02 +0100

    Quiz

---
+++ questions.py
@@ -0,0 +1,31 @@
+# Define the quiz questions as a list of dictionaries
+# Git Quiz
+
+quiz_questions = [
+    {
+        'question': 'What is the output of 3 + 4?',
+        'options': ['7', '8', '12', 'None of the above'],
+        'answer': '7'
+    },
+    {
+        'question': 'Python is an interpreted language. (True/False)',
+        'options': ['True', 'False'],
+        'answer': 'True'
+    },
+    {
+        'question': 'Which data type is mutable in Python?',
+        'options': ['int', 'str', 'list', 'tuple'],
+        'answer': 'list'
+    },
+    {
+        'question': 'What is the maximum length of a Python identifier?',
+        'options': ['32','16','128','None of the above'],
+        'answer': 'None of the above'
+    },
+    {
+        'question': 'How is a code block indicated in Python?',
+        'options': ['Bracket','Indentation','Key','None of above'],
+        'answer': 'Indentation'
+    }
+]
+
